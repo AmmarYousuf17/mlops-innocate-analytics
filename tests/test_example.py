0ff323aef0e16555170f8ca5f593436b912cf800
AmmarYousuf17 <ammaryousuf177@gmail.com>
2025-05-11 23:07:30 +0500

    Add example test case

---
+++ tests/test_example.py
@@ -0,0 +1,9 @@
+# tests/test_example.py
+
+# A simple test case to check addition
+def test_addition():
+    assert 1 + 1 == 2
+
+# Another test case to check subtraction
+def test_subtraction():
+    assert 5 - 3 == 2
